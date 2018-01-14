abfe67d91b242c96236df3387c30f0e32ce6bcc7
zrh <qq798435167@gmail.com>
2018-01-14 21:59:03 +0800

    update13

---
+++ success.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# coding: utf-8
+"""
+--------------------------------------------------
+    File Name   : success.py
+    Description :
+    Author      : zrh
+    Date        : 2018/1/14
+--------------------------------------------------
+"""
+__author__ = 'zrh'
+
+print('success!')
