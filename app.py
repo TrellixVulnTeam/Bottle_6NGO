ec61b53b85545baa9c96195cbb5da41a80036b3f
zrh <qq798435167@gmail.com>
2018-01-14 16:11:27 +0800

    init

---
+++ app.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python
+# coding: utf-8
+"""
+--------------------------------------------------
+    File Name   : app.py
+    Description :
+    Author      : zrh
+    Date        : 2018/1/13
+--------------------------------------------------
+"""
+__author__ = 'zrh'
+
+import sys
+from bottle import abort, run, route, request, HTTPResponse, error
+
+
+@route('/abort', method='POST')
+def do_abort():
+    data = request.body
+    data = data.read().decode('utf-8')
+    print(data)
+    if data != 'abort':
+        abort(400,'your request is not abort')
+    return 'abort test'
+
+
+@route('/httpresponse',method='POST')
+def do_httpresponse():
+    data = request.body
+    data = data.read()
+    if data!='httpresponse':
+        return HTTPResponse(body='your request is not httpresponse',status=400)
+    return 'httpresponse test'
+
+
+@route('/code',method='POST')
+def do_code():
+    data = request.body
+    data = data.read()
+    if data != 'code':
+        abort(499,'your request is not code')
+    return 'code test'
+
+
+@error(499)
+def error499(error):
+    return error.body
+
+
+def main():
+    run(server='paste',host='0.0.0.0',port=9001)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ venv/Scripts/bottle.py
@@ -0,0 +1,4 @@
+#!d:\users\msi\pycharmprojects\bottledemo\venv\scripts\python.exe
+# EASY-INSTALL-SCRIPT: 'bottle==0.12.13','bottle.py'
+__requires__ = 'bottle==0.12.13'
+__import__('pkg_resources').run_script('bottle==0.12.13', 'bottle.py')
